8522c7f079d85006e487a866e218420f003d777c
Yashika <yb310jh@gmail.com>
2022-12-15 17:09:33 +0530

    Intial Flights And Search Service setup included required dependencies, added .env file

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the FlightsAndSearch service for the Airline Management Project.
+Project Setup
+1. Clone the project
+2. Run ```npm install``` in the command propmt
+3. Create ```.env``` file outside src and write the PORT number.
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+const {PORT}=require('./config/serverConfig.js');
+const serverSetup = async() => {
+    const app=express();
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended: true}));
+    app.listen(PORT,()=>{
+        console.log(`Server started at PORT ${PORT}`);
+    })
+}
+serverSetup();
